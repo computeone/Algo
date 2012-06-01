39906b046acccad30eb9082b90a4bce8a5efd461
niubaisui <1216936151@qq.com>
2012-06-01 23:28:54 +0800

    全部提交。

---
+++ Algo/src/main/java/foo/Graph_matrix.java
@@ -0,0 +1,23 @@
+package foo;
+
+//图的矩阵表示及其相关算法
+public class Graph_matrix {
+	private Integer[][] matrix;
+	public Graph_matrix(Integer[][] matrix){
+		this.matrix=matrix;
+	}
+	
+	public void setGraph(Integer[][] relation){
+		for(int i=0;i<matrix.length;i++){
+			for(int j=0;j<matrix.length;j++){
+				matrix[i][j]=0;
+			}
+			int k=relation[i].length;
+			for(int m=0;m<k;m++){
+				int index=relation[i][m];
+				matrix[i][index]=1;
+			}
+		}
+	}
+
+}
---
+++ Algo/src/main/java/foo/Netflow.java
@@ -0,0 +1,14 @@
+package foo;
+
+//网络流算法
+public class Netflow {
+	//增广路经流算法
+	public void compute(){
+		
+	}
+	//
+	public void processPath(){
+		
+	}
+
+}
